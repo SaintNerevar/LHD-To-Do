49caa01232b0a2eb23123d3983e7aaee393d517f
SaintNerevar <shivahegde@rocketmail.com>
2021-01-17 16:06:36 +0530

    Refactor: Two separate routes for create and delete

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -5,16 +5,24 @@ from app.models.task import Task
 
 blueprint = Blueprint('home', __name__)
 
-@blueprint.route('/', methods=('GET', 'POST'))
+@blueprint.route('/')
 def index():
-    if request.method == 'GET':
-        return render_template('home/index.html')
-    elif request.method == 'POST':
-        if request.form['task-desc']:
-            task_desc = request.form['task-desc']
-            user_id = g.user.id
-            Task.add_task(task_desc, user_id)
-            return render_template('home/index.html')
-        else:
-            flash('A task can\'t be blank, boss!', 'info')
-            return render_template('home/index.html')
+    return render_template('home/index.html')
+
+@blueprint.route('/create', methods=['POST'])
+def create():
+    if request.form['task-desc']:
+        task_desc = request.form['task-desc']
+        user_id = g.user.id
+        Task.add_task(task_desc, user_id)
+        return redirect(url_for('home.index'))
+    else:
+        flash('A task can\'t be blank, boss!', 'info')
+        return redirect(url_for('home.index'))
+
+@blueprint.route('/delete', methods=['POST'])
+def delete():
+    task_id = request.form['task-id']
+    Task.delete_task(task_id)
+    return redirect(url_for('home.index'))
+    
