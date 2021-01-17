3f715fec06be41e49339e54dfed77a48d0029108
SaintNerevar <shivahegde@rocketmail.com>
2021-01-17 16:07:53 +0530

    Make add_task and delete_task static

--- app/models/task.py
+++ app/models/task.py
@@ -1,17 +1,23 @@
 from app.extensions import db
-from datetime import datetime
+from datetime import date
 
 class Task(db.Model):
     __tablename__ = 'task'
 
     id = db.Column(db.Integer(), primary_key=True)
     description = db.Column(db.String(80), nullable=False)
-    create_date = db.Column(db.DateTime, nullable=False, default=datetime.date)
+    create_date = db.Column(db.DateTime, nullable=False, default=date.today())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def add_task(self, task_desc, user_id):
+    @staticmethod
+    def add_task(task_desc, user_id):
         task = Task(description=task_desc, user_id=user_id)
         db.session.add(task)
+        db.session.commit()
+
+    @staticmethod
+    def delete_task(task_id):
+        Task.query.filter_by(id=task_id).delete()
         db.session.commit()
 
     def __repr__(self):
